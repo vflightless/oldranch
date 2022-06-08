97dd9b111916ddd7fc93a54c8ca8a1836c948f20
vflightless <vanderhorstjacob@gmail.com>
2022-06-07 22:59:33 -0700

    xd

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template class="h-100">
+    <section class="d-block h-100 w-100" :style="{ backgroundImage: `url(${bgImage})`}" style="background-size: cover;" >
+        <b-container class="d-inline-block h-100">Get Fuqed</b-container>
+        <h1>Default</h1>
+        <Nuxt />
+    </section>
+</template>
+
+<script>
+import bgImg from "~/assets/chbg-upscale.png";
+
+export default {
+    layout: 'default',
+    data() {
+        return { bgImage: bgImg }
+    }
+}
+</script>
