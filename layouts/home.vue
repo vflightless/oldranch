f0ae4b6bae1c95359fcd877fbc124c28a9950dbc
vflightless <vanderhorstjacob@gmail.com>
2022-06-10 16:23:23 -0700

    xd

---
+++ layouts/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <section style="width: 100vw; height: 100vh; background-repeat: no-repeat; background-size: cover; background-position: right;" :style="{ backgroundImage: `url(${bgUrl})` }">
+        <Nuxt />
+    </section>
+</template>
+
+<script>
+import bgUrl from '~/assets/chbg-upscale.png';
+
+export default {
+    layout: 'home',
+    data() { return { bgUrl } },
+}
+</script>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        <Navbar />
+        <b-container class="mx-auto" fluid>
+            Coming Soon...
+        </b-container>
+    </section>
+</template>
+
+<script>
+export default {
+    
+    layout: 'default',
+    components: { navbar }
+}
+</script>
